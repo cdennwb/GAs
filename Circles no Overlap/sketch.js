037a7f96173e480e2d921decbc13331012933377
Caeden Newby <cdennwb@gmail.com>
2018-03-03 14:25:49 -0800

    Commited by Atom.

--- Circles no Overlap/sketch.js
+++ Circles no Overlap/sketch.js
@@ -1,38 +1,56 @@
-var circles = [];
+var circle = function(_x, _y, _r){
+  var circ = {
+    x: _x,
+    y: _y,
+    r: _r
+  };
+  return circ;
+}
 
-function setup() {
-  createCanvas(500, 500);
-  for(var i = 0; i < 500; i++){
-    var circle = {
-      x: random(500),
-      y: random(500),
-      r: 0
-    }
-    var overlap = false;
-    while(circle.r < 250 && !overlap){
-      console.log(circle.r);
-      for (var j = 0; j < circles.length; j++){
-        temp = circles[j];
-        d2 = dist(circle.x, circle.y, temp.x, temp.y);
-        if(d2 < circle.r + temp.r || circle.x + circle.r > 500){
-          overlap = true;
-          console.log(2);
-          break;
-        }
-      }
-      circle.r = circle.r + 1;
-    }
-    if(circle.r > 5){
-      circle.r = circle.r - 1;
-      circles.push(circle);
-    }
-  }
-  for(var i = 0; i < circles.length; i++){
-    noFill();
-    ellipse(circles[i].x, circles[i].y, circles[i].r*2, circles[i].r*2);
+var allCircles = [];
+
+var display = function(shapes){
+  clear();
+  for(var i = 0; i < shapes.length; i++){
+    ellipse(shapes[i].x, shapes[i].y, shapes[i].r*2, shapes[i].r*2);
   }
 }
 
-function draw() {
+var notInside(circ, circles){
+  for(var i = 0; i < circles.length; i++){
+    if(circ.x < circles[i].x - circles[i].r
+       && circ.y < circles[i].y - circles[i].r
+       && circ.x > circles[i].x + circles[i].r
+       && circ.y > circles[i].y + circles[i].r){
+         return false;
+       }
+  }
+  return true;
+}
 
+var notTouching(circ, circles){
+  for(var i = 0; i < circles.length; i++){
+    if(dist(circ.x, circ.y, circles[i].x, circles[i].y) < circ.r + circles[i].r){
+      return false;
+    }
+  }
+  return true;
+}
+var manageCircs = function(circles){
+  var newCircle = circle(random(windowWidth), random(windowHeight), 0);
+  if(notInside(newCircle, circles)){
+    while(notTouching){
+      newCircle.r += 1;
+      ellipse(newCircle.x, newCircle.y, newCircle.r*2, newCircle.r*2);
+      display(circles);
+    }
+    circles.push(newCircle);
+  }
+}
+
+function setup(){
+  createCanvas(windowWidth, windowHeight);
+}
+function draw(){
+  manageCircs(allCircles);
 }
